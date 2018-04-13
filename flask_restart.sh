4d623abb61753feba3abe21ba0de29878ab8c708
longfengpili <398745129@qq.com>
2018-04-13 09:19:51 +0800

    '优化chongqi'

--- flask_restart.sh
+++ flask_restart.sh
@@ -3,4 +3,4 @@
 
 ps -ef | grep flask_run.py | awk '{print $2}'|xargs kill -9
 
-nohup python3 /usr/longfengpili/loggly/word/word_run.py >myhup & 
+nohup python3 /usr/longfengpili/loggly/flasknet/flask_run.py >myhup & 
